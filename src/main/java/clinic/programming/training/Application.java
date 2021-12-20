41778e0d8fcaf67a1870c0f3f2dea19a447a4f8a
sahbihabli1983 <sahbi.habli2021@gmail.com>
2021-12-20 18:27:22 +0100

    Modified Application file

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,18 @@
 package clinic.programming.training;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
+	
+	public void greet(){
+	List<String> list =new ArrayList<>();
+	list.add("Hello");
+		for(String l: list){
+			System.out.println("Greeting: "+l);
+		}
+			
+	}
     
     public Application() {
         System.out.println ("Inside Application");
@@ -9,6 +21,7 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+		Application app = new Application();
+		app.greet();
     }
 }
